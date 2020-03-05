43715424c085615205b3e04d0481785862efa723
UmbraVenus <60438078+UmbraVenus@users.noreply.github.com>
2020-03-05 17:24:51 -0600

    Interactive
    From practice I found out that I lack in knowledge of algorithm and data structures, choosing this book to study and hopefully it will go well

---
+++ NotesBuiltInTypes.py
@@ -0,0 +1,27 @@
+# Built in Collection Data Type
+#############################################
+""" List, ordered collection, [], heterogeneous
+       indexing        []
+       concatenation   +
+       repeition       *
+       membership      in
+       length          len
+        slicing         [:], does not include the second part
+       more etc, on documentation, a lot
+"""
+#############################################
+# Strings are immutable!!!
+#############################################
+""" Tuples, like list, but immutable
+        myTuple = (2, True, 4.96)
+"""
+############################################
+""" Set is an UNORDERED collection of IMMUTABLE data objects
+    NO DUPLICATE!!!! {} 
+    Look more on operations on 2 sets
+"""
+############################################
+""" Dictionary(Map)
+        Unordered, 
+        phoneext = {'David': 1410, 'brad': 1137}
+"""
