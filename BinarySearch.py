58b61d6be5e696f0dcf316bd43dc817717df4caf
UmbraVenus <60438078+UmbraVenus@users.noreply.github.com>
2020-03-09 22:14:34 -0500

    BinarySearch
    Concept Implemented

---
+++ BinarySearch.py
@@ -0,0 +1,17 @@
+def binarySearch(alist, item):
+    if len(alist) == 0:
+        return False
+    else:
+        mid = len(alist) // 2
+        if alist[mid] == item:
+            return True
+        elif alist[mid] > item:
+            return binarySearch(alist[:mid], item)
+        else:
+            return binarySearch(alist[mid + 1:], item)
+
+
+testlist = [0, 1, 2, 8, 13, 17, 19, 32, 42, ]
+print(binarySearch(testlist, 3))
+print(binarySearch(testlist, 13))
+print(binarySearch(testlist, 42))
